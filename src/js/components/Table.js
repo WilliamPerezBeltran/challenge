a6e8f2c2de7e6b1eba70b4ec5349580262c4f9c6
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-07-24 19:30:53 -0500

    Add table basic component

---
+++ src/js/components/Table.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  render() {
+    return (
+      <div>
+        <p>Simple table with header</p>
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Comercio</th>
+            <th>Cuit</th>
+            <th>Concepto 1</th>
+            <th>Concepto 2</th>
+            <th>Concepto 3</th>
+            <th>Concepto 4</th>
+            <th>Concepto 5</th>
+            <th>Concepto 6</th>
+            <th>Balance actual</th>
+            <th>Activo</th>
+            <th>Última versión </th>
+          </tr>
+          <tr>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+            <td>xxxxxxx</td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table;
