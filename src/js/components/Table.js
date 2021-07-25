adbf80fc081d6cd2cd0166c6d4222a211973c2ce
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-07-24 22:20:36 -0500

    Prevent Warning: validateDOMNesting with table

--- src/js/components/Table.js
+++ src/js/components/Table.js
@@ -31,41 +31,43 @@ class Table extends Component {
       <div>
         <p>Business information table</p>
         <table>
-          <tr>
-            <th>Id</th>
-            <th>Comercio</th>
-            <th>Cuit</th>
-            <th>Concepto 1</th>
-            <th>Concepto 2</th>
-            <th>Concepto 3</th>
-            <th>Concepto 4</th>
-            <th>Concepto 5</th>
-            <th>Concepto 6</th>
-            <th>Balance actual</th>
-            <th>Activo</th>
-            <th>Última venta </th>
-          </tr>
+          <tbody>
+            <tr>
+              <th>Id</th>
+              <th>Comercio</th>
+              <th>Cuit</th>
+              <th>Concepto 1</th>
+              <th>Concepto 2</th>
+              <th>Concepto 3</th>
+              <th>Concepto 4</th>
+              <th>Concepto 5</th>
+              <th>Concepto 6</th>
+              <th>Balance actual</th>
+              <th>Activo</th>
+              <th>Última venta </th>
+            </tr>
 
-          {this.state.trades
-            .slice(0, this.state.total)
-            .map((trade, tradeId) => {
-              return (
-                <tr key={tradeId}>
-                  <td>{trade.id}</td>
-                  <td>{trade.trade}</td>
-                  <td>{trade.cuit}</td>
-                  <td>{trade.concept1}</td>
-                  <td>{trade.concept2}</td>
-                  <td>{trade.concept3}</td>
-                  <td>{trade.concept4}</td>
-                  <td>{trade.concept5}</td>
-                  <td>{trade.concept6}</td>
-                  <td>{trade.actualBalance}</td>
-                  <td>{trade.active ? 1 : 0}</td>
-                  <td>{trade.lastSale}</td>
-                </tr>
-              );
-            })}
+            {this.state.trades
+              .slice(0, this.state.total)
+              .map((trade, tradeId) => {
+                return (
+                  <tr key={tradeId}>
+                    <td>{trade.id}</td>
+                    <td>{trade.trade}</td>
+                    <td>{trade.cuit}</td>
+                    <td>{trade.concept1}</td>
+                    <td>{trade.concept2}</td>
+                    <td>{trade.concept3}</td>
+                    <td>{trade.concept4}</td>
+                    <td>{trade.concept5}</td>
+                    <td>{trade.concept6}</td>
+                    <td>{trade.actualBalance}</td>
+                    <td>{trade.active ? 1 : 0}</td>
+                    <td>{trade.lastSale}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
         </table>
       </div>
     );
